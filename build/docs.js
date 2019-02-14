276e22e10698b5878fabdc93b98f2bae117c4695
jrf <yyn@126mail.cn>
2019-02-14 18:40:35 +0800

    npm script 跨平台待解决

---
+++ build/docs.js
@@ -0,0 +1,6 @@
+const { rm, cp, mkdir, exec, echo } = require('shelljs');
+const chalk = require('chalk');
+
+// exec('jsdoc ./**/*.js -d ./docs/jsdocs');
+// exec('echo $npm_package_name')
+exec('node **/a.js')
