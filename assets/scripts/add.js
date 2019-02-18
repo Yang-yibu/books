e1e22127cb57142929068eb3e7eeead108a02d7c
Yang-yibu <18335834527@163.com>
2019-02-19 07:48:29 +0800

    FE 小架构- 添加页 骨架：使用 Jquery ES6 渲染 1

--- assets/scripts/add.js
+++ assets/scripts/add.js
@@ -1,6 +1,25 @@
-var app6 = new Vue({
-  el: '#app-6',
-  data: {
-    message : 'Hello Vue'
+
+// 使用 Vue 渲染
+// var app6 = new Vue({
+//   el: '#app-6',
+//   data: {
+//     message : 'Hello Vue'
+//   }
+// })
+
+// 使用 JQuery
+class Create {
+  constructor() {
+    this.btn = $('#js-btn')
   }
-})
+
+  fn() {
+    this.btn.click(function () {
+      alert(1);
+    })
+  }
+}
+
+const s = new Create();
+s.fn();
+// export default Create;
